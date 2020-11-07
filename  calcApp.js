d21bb1f899fca000ae3fc6d937d9524d6519d072
nerdboy <saintofsteel@me.com>
2020-11-07 09:51:19 -0500

    Start Logic for JS

---  calcApp.js
+++  calcApp.js
@@ -1,2 +1,16 @@
 const boxes = Array.from(document.getElementsByClassName("box"));
 const mathBox = Array.from(document.getElementsByClassName("boxMath"));
+const displayBox = document.getElementById("displayBox");
+
+//Logic for display on calculator
+
+let displayNum;
+
+// when numbers get pressed. This will most likely go in a function. 
+displayNum = (displayNum * 10) + numPress;
+ 
+// didplayed number being held until calculation gets made 
+let numHold;
+
+displayBox.innerText = `54321`;
+
